038fe7a6c01cf466684955abd61f29a9de30ca77
Willem van den Ende <willem@qwan.eu>
2015-12-25 10:03:50 +0100

    Extract handler function for exceptions
    to check what type declaration would look like.
    
    Should help with wishful thinking for handler function in FB.once call.

--- src/Exceptional.purs
+++ src/Exceptional.purs
@@ -1,8 +1,8 @@
 module Exceptional where
 import Prelude
 import Control.Monad.Eff
-import Control.Monad.Eff.Console (log)
-import Control.Monad.Eff.Exception (EXCEPTION(), error, throwException, catchException, message)
+import Control.Monad.Eff.Console (log, CONSOLE())
+import Control.Monad.Eff.Exception (EXCEPTION(), error, Error(), throwException, catchException, message)
 
 --https://github.com/purescript/purescript-exceptions/blob/master/docs/Control/Monad/Eff/Exception.md
 --trying to understand purescript book p 105 , handlers and actions
@@ -11,9 +11,11 @@ readConfig :: forall eff. Eff (err :: EXCEPTION | eff) Unit
 readConfig = throwException exception
   where exception = error "Throwing a Javascript exception"
 
+printException :: forall eff. Error -> Eff (console :: CONSOLE | eff) Unit
+printException e = do
+  log (message e)
+
+playWithExceptions :: forall eff. Eff ( console :: CONSOLE | eff ) Unit
 playWithExceptions = do
   log "exceptions coming up next"
   catchException printException readConfig
-  where 
-    printException e = do
-      log (message e)
