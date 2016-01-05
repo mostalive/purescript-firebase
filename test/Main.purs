8a1dba770bf03d6c0b598cb903b768d57368318d
Willem van den Ende <willem@qwan.eu>
2016-01-05 12:23:13 +0000

    Initial test library

--- test/Main.purs
+++ test/Main.purs
@@ -1,9 +1,26 @@
 module Test.Main where
 
 import Prelude
+
+import Control.Monad.Aff
 import Control.Monad.Eff
 import Control.Monad.Eff.Console
+import Test.Spec                  (describe, it)
+import Test.Spec.Runner           (Process(), run)
+import Test.Spec.Assertions       (shouldEqual)
+import Test.Spec.Reporter.Console (consoleReporter)
 
-main :: forall e. Eff (console :: CONSOLE | e) Unit
-main = do
-  log "You should add some tests."
+main ::  forall eff. Eff ( console :: CONSOLE, process :: Process | eff) Unit
+main = run [consoleReporter] do
+  describe "purescript-spec" do
+    describe "What is it?" do
+      it "awesome" do
+        let isAwesome = true
+        isAwesome `shouldEqual` true
+    describe "Features" do
+      it "runs in NodeJS" $ return unit
+      it "runs in the browser" $ return unit
+      it "supports async specs" do
+        res <- later' 100 $ return "Alligator"
+        res `shouldEqual` "Alligator"
+      it "is PureScript 0.7 compatible" $ return unit
