ce59511352b50e59d501c767f749cbd07ab08e3e
Willem van den Ende <willem@qwan.eu>
2016-02-03 21:08:45 +0000

    WIP Logged in status with UserCredentials

--- test/AuthDataSpec.purs
+++ test/AuthDataSpec.purs
@@ -14,13 +14,16 @@ import Data.Generic
 type TweetSpec = forall eff. Spec (eff) Unit
 -- convert authdata (from twitter to start with) to a purescript authdata record
 -- sample data at https://boiling-heat-7831.firebaseapp.com/authspike.html
-
-record :: { provider :: String, uid :: String, twitter :: {}
+newtype UserCredentials = UserCredentials {
+          provider :: String
+          , uid :: String
           , token :: String
           , auth :: {}
           , expires :: Int
           }
-record = { provider: "twitter", uid: "twitter:16594263", twitter: {}, token: "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ…", auth: {}, expires: 1454431083 }
+
+record :: UserCredentials
+record = UserCredentials { provider: "twitter", uid: "twitter:16594263", token: "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ…", auth: {}, expires: 1454431083 }
 
 -- twitter has: displayName: "Willem van den Ende, id: 165 etc, username: mostalive, profileImageUrl
 -- and accessToken and accessTokenSecret. not sure if we need those.
@@ -28,7 +31,15 @@ record = { provider: "twitter", uid: "twitter:16594263", twitter: {}, token: "ey
 
 -- ProviderUserProfile = TwitterProfile TwitterProfileRecord | GoogleProfile GoogleProfileRecord | etc
 
-data AuthenticationStatus = LoggedOut | LoggedIn -- | LoggedIn UserCredentials ProviderUserProfile
+data AuthenticationStatus = LoggedOut | LoggedIn UserCredentials -- | LoggedIn UserCredentials ProviderUserProfile
+
+derive instance genericUserCredentials :: Generic UserCredentials
+
+instance showUserCredentials :: Show UserCredentials where
+  show = gShow
+
+instance eqUserCredentials :: Eq UserCredentials where
+  eq = gEq
 
 derive instance authenticationStatusIsGeneric :: Generic AuthenticationStatus
 
@@ -38,9 +49,16 @@ instance authenticationShow :: Show AuthenticationStatus where
 instance authenticationeq :: Eq AuthenticationStatus where
   eq = gEq
 
+
 instance authenticationStatusIsForeign :: IsForeign AuthenticationStatus where
   read value | isNull value  = return LoggedOut
-  read value = return LoggedIn
+  read value = do
+    provider <- readProp "provider" value
+    uid <- readProp "uid" value
+    token <- readProp "token" value
+    expires <- readProp "expires" value
+    let auth = {}
+    return $ LoggedIn (UserCredentials {provider, uid, token, auth, expires })
 
 
 
@@ -57,7 +75,9 @@ authDataSpec = do
       let nullStatus = readJSON twitterLoggedInJson :: Either ForeignError AuthenticationStatus
       case nullStatus of
         Left e -> throwError $ error $ show e
-        Right s -> s `shouldEqual` LoggedIn
+        Right s -> s `shouldEqual` (LoggedIn record)
 
--- approximation. The empty objects have a lot of provider specific data in them (the 'twitter' field has user profile picture etc
+-- approximation. The empty objects have a lot of provider specific data in them
+-- e.g. the 'twitter' field has user profile picture etc. different for google etc.
+-- so watch out when building a parser for that part.
 twitterLoggedInJson = """{ "provider": "twitter", "uid": "twitter:16594263", "twitter": {}, "token": "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ…", "auth": {}, "expires": 1454431083 }"""
