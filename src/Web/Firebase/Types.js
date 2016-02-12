629dc52b1e1c5535ad20d1b935e3dc0eb9677763
Willem van den Ende <willem@qwan.eu>
2016-02-12 16:20:48 +0000

    Fix: forgot to commit new Types.js

---
+++ src/Web/Firebase/Types.js
@@ -0,0 +1,11 @@
+
+"use strict";
+
+// module Web.Firebase.Types
+
+exports.firebaseErrToString = function(fberr) {
+ var message = fberr.message + "\n | firebase code: | \n " + fberr.code
+ if(fberr.details) // abuse truthyness of null and undefined
+   message += "\n | details | \n" | fberr.details
+ return message;
+};
