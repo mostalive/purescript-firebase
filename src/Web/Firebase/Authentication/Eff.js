7179817a5cd97b7f307d0436f01ef731b9ca34c7
Willem van den Ende <willem@qwan.eu>
2016-03-01 16:19:09 +0000

    Fix - process effects in onAuth

--- src/Web/Firebase/Authentication/Eff.js
+++ src/Web/Firebase/Authentication/Eff.js
@@ -2,9 +2,13 @@
 
 // module Web.Firebase.Authentication.Eff
 
+
 exports._onAuth = function (callback, firebase) {
+  var cbEffect = function(data) {
+    return cbEffect(data)(); // ensure effect gets used
+  };
   return function() {
-    return firebase.onAuth(callback);
+    return firebase.onAuth(cbEffect);
   };
 };
 
