2cfb356ebee94b04179f01be862d9d7491c03f73
Willem van den Ende <willem@qwan.eu>
2016-03-11 11:33:43 +0000

    Test for hacked authtoken in Aff

--- test/Authentication.purs
+++ test/Authentication.purs
@@ -15,8 +15,8 @@ import Web.Firebase (EventType(..),once, pushE, push)
 import Test.Spec                  (describe, it, Spec())
 import Test.Spec.Assertions       (shouldEqual)
 import Test.Spec.Assertions.Aff (expectError)
-import Web.Firebase.Monad.Aff (onceValue, on, firebaseErrToString)
 import Web.Firebase.Authentication.Eff (authWithCustomToken)
+import Web.Firebase.Authentication.Aff as AuthAff
 
 authenticationSpec :: forall eff. FBT.Firebase -> Spec (firebase :: FBT.FirebaseEff, err :: EXCEPTION, avar :: AVAR, console :: CONSOLE | eff ) Unit
 authenticationSpec forbiddenRef = do
@@ -31,4 +31,6 @@ authenticationSpec forbiddenRef = do
                                  forbiddenRef
           actual <- takeVar respVar
           actual `shouldEqual` "INVALID_TOKEN: Could not parse auth token.\n | firebase code: | \n INVALID_TOKEN"
-
+      describe "with Aff" do
+        it "on fake authentication throws an error" do
+          expectError $ AuthAff.authWithCustomToken "faketoken" forbiddenRef
