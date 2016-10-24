f0e2c1647a38584d12ae00012780b0ee55aa5de8
Willem van den Ende <willem@qwan.eu>
2016-10-24 12:47:17 +0100

    Delete pending tests :refactoring:

--- test/DataSnapshotSpec.purs
+++ test/DataSnapshotSpec.purs
@@ -12,7 +12,7 @@ import Web.Firebase.Aff (onceValue)
 import Web.Firebase.UnsafeRef (refFor)
 import Web.Firebase.DataSnapshot as D
 import Web.Firebase.Types as FBT
-import Test.Spec                  (describe, pending, it, Spec())
+import Test.Spec                  (describe, it, Spec())
 import Test.Spec.Assertions       (fail, shouldEqual)
 
 eSnapshot :: forall eff. Aff (firebase :: FBT.FirebaseEff | eff) FBT.DataSnapshot
@@ -38,35 +38,15 @@ dataSnapshotSpec snapshot =
       it "can tell us the number of children" do
         let numChildren = D.numChildren snapshot
         expect (numChildren >= 1)
-      pending "can list the keys of its children by using forEach"
 
       it "can tell us a child does not exist" do
         (D.hasChild snapshot "doesnotexist")  `shouldEqual` false
-      pending "relying on a fixed existing child is too fragile"
-      pending "it can tell us a child exists"
-      pending "it can give us a snapshot of one of its children"
-{-
-      it "can tell us a child exists" do
-        - use foreach instead. perhaps create list like interface in Web.Firebase.Extra.DataSnapshotList
-        expect $ D.hasChild snapshot "-K7GbWeFHfJXlun7szRe"
-
-      it "can give us a snapshot of one of its children" do
-        let c = D.child snapshot "-K7GbWeFHfJXlun7szRe"
-        expect $ D.exists c
--}
       it "can tell us the location at the snapshot exists" do
         expect $ (D.exists snapshot)
 
       it "can tell us it has children" do
         expect $ D.hasChildren snapshot
 
-      pending "says the key of the database root is Nothing" {-
-        -- Root has become inacessible due to permission tests :-( not sure how to test Nothing for key now
-        do
-        rs <- rootSnapshot
-        let key = D.key rs
-        key `shouldEqual` Nothing
-      -}
       it "says the key of /entries is entries" do
         let key = D.key snapshot
         key `shouldEqual` (Just "entries")
@@ -74,7 +54,3 @@ dataSnapshotSpec snapshot =
       it "it can not tell us the location at the snapshot does not exist" do
         let noChild = (D.child snapshot "doesnotexist")
         (D.exists noChild) `shouldEqual` false
-        -- /entries/doesnotexist
-        -- perhaps it can now, it might just have been an error callback with an additional () missing.
-      pending "can it say the value of child \"entries\" is Nothing?"
-        -- this relies on trying to read a firebase ref with once, and that 'works' by never being called back
