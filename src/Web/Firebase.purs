53a26853d556b8ae9b320bb628d5e5d5f2dd5ef5
Willem van den Ende <willem@livingsoftware.co.uk>
2017-08-15 11:38:55 +0100

    Remove unused $

--- src/Web/Firebase.purs
+++ src/Web/Firebase.purs
@@ -16,7 +16,7 @@ module Web.Firebase
 )
 where
 
-import Prelude (class Show, class Eq, class Ord, Unit, (<$>), (<<<), compare, map, ($))
+import Prelude (class Show, class Eq, class Ord, Unit, (<$>), (<<<), compare, map)
 import Control.Monad.Eff (Eff())
 import Data.Foreign (Foreign())
 import Data.Function.Uncurried (Fn1(), Fn2(), Fn3(), Fn4(), runFn1, runFn2, runFn3, runFn4)
