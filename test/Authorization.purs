375dc774b3047734c9809f26338e7a9ea28f8175
Willem van den Ende <willem@livingsoftware.co.uk>
2018-04-24 23:07:35 +0100

    Refactor Reveal more intentions in authorizationSpec
    more one liners, shows what really changes between tests and what does not.

--- test/Authorization.purs
+++ test/Authorization.purs
@@ -4,9 +4,9 @@ import Control.Monad.Aff (Aff, attempt)
 import Control.Monad.Eff.Exception (message)
 import Data.Either (either)
 import Data.Foreign (toForeign)
-import Prelude (Unit, bind, discard, ($))
+import Prelude (Unit, bind, discard, ($), (<<<), (>>=))
 import Test.Spec (Spec, describe, it)
-import Test.Spec.Assertions (shouldEqual)
+import Test.Spec.Assertions (fail, shouldEqual)
 import Test.Spec.Assertions.Aff (expectError)
 import Web.Firebase (EventType(ChildMoved, ChildChanged, ChildRemoved, ChildAdded))
 import Web.Firebase.Aff (mkEventAtLocation, mkSaveable)
@@ -18,33 +18,37 @@ forbiddenR = FAff.child "forbidden"
 
 authorizationSpec :: forall eff. FBT.Firebase -> Spec (firebase :: FBT.FirebaseEff | eff) Unit
 authorizationSpec ref = do
-  describe "Authorization" do
+  describe "Authorization with Aff at forbidden location throws an error" do
     describe "Writing" do
-      let rr = forbiddenR ref
-      it "with Aff push on forbidden location throws an error" do
+      it "push" do
         forbiddenRef <- forbiddenR ref
         let newValue = {success: "push Aff"}
         expectError $ FAff.push $ mkSaveable (toForeign newValue) forbiddenRef
-    describe "once() on forbidden location" do
+    describe "once" do
       it "with Aff throws an error" do
         forbiddenRef <- forbiddenR ref
-        e <- attempt $ FAff.onceValue forbiddenRef  -- catch error thrown and assert
-        either (\err -> (message err) `shouldEqual` "permission_denied at /forbidden: Client doesn't have permission to access the desired data.\n | firebase code: | \n PERMISSION_DENIED") (\_ -> "expected an error to be thrown" `shouldEqual` "but was not") e
-    describe "on() at forbidden location" do
+        (attempt $ FAff.onceValue forbiddenRef) >>= expectPermissionDeniedError
+    describe "on" do
       it "ChildAdded with Aff throws an error" do
-        forbiddenRef <- forbiddenR ref
-        expectError $ FAff.on $ mkEventAtLocation ChildAdded forbiddenRef
-      it "ChildRemoved with Aff throws an error" do
-        forbiddenRef <- forbiddenR ref
-        expectError $ FAff.on $ mkEventAtLocation ChildRemoved forbiddenRef
-      it "ChildChanged with Aff throws an error" do
-        forbiddenRef <- forbiddenR ref
-        expectError $ FAff.on $ mkEventAtLocation ChildChanged forbiddenRef
-      it "ChildMoved with Aff throws an error" do
-        forbiddenRef <- forbiddenR ref
-        expectError $ FAff.on $ mkEventAtLocation ChildMoved forbiddenRef
+        expectPermissionDeniedErrorOn ChildAdded
+      it "ChildRemoved" do
+        expectPermissionDeniedErrorOn ChildRemoved
+      it "ChildChanged" do
+        expectPermissionDeniedErrorOn ChildChanged
+      it "ChildMoved" do
+        expectPermissionDeniedErrorOn ChildMoved
     it "set() with Aff at forbidden location throws an error" do
       forbiddenRef <- forbiddenR ref
       let newValue = {success: "set Aff"}
       e <- attempt $ FAff.set $ mkSaveable (toForeign newValue) forbiddenRef
-      either (\err -> (message err) `shouldEqual` "PERMISSION_DENIED: Permission denied\n | firebase code: | \n PERMISSION_DENIED") (\_ -> "expected an error to be thrown" `shouldEqual` "but was not") e
+      either (\err -> (message err) `shouldEqual` "PERMISSION_DENIED: Permission denied\n | firebase code: | \n PERMISSION_DENIED")
+             (\_ -> "expected an error to be thrown" `shouldEqual` "but was not")
+             e
+    where
+      expectPermissionDeniedErrorOn eventType =
+        forbiddenR ref >>= expectError <<< FAff.on <<< mkEventAtLocation eventType
+
+      expectPermissionDeniedError attempted =
+        either (\err -> (message err) `shouldEqual` "permission_denied at /forbidden: Client doesn't have permission to access the desired data.\n | firebase code: | \n PERMISSION_DENIED")
+               (\_ -> fail "expected a permission_denied error to be thrown, but was not")
+               attempted
