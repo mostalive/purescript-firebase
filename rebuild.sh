3acf3a95943b662ddd7f564b1b3f76a1dd3dc94d
Willem van den Ende <willem@qwan.eu>
2020-03-02 11:35:21 +0000

    Update .nvmrc to v11.10.1
    Downgraded 'rebuild' to just compile for now, so we can get to
    zero warnings for missing imports.

--- rebuild.sh
+++ rebuild.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
+# run `nvm use` before starting this script to set the common node version
+# https://github.com/nvm-sh/nvm/blob/master/README.md
 # Bash Strict Mode http://redsymbol.net/articles/unofficial-bash-strict-mode/
 set -euo pipefail
 IFS=$'\n\t'
+
 
 ./clean.sh
 rm -rf node_modules
 rm -rf bower_components
 npm install
-bower install
-pulp test
+npx bower install
+npm run build
 
