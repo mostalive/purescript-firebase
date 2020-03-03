4807d358c85aac523d5314c74b7c12f881c596f9
Willem van den Ende <willem@qwan.eu>
2020-03-03 14:41:57 +0000

    Add initializeTestApp and initalizeAnonymousTestApp

--- src/Web/Firebase/Testing.js
+++ src/Web/Firebase/Testing.js
@@ -1,13 +1,38 @@
 firebase = require("@firebase/testing");
 
+// Callback to use this when cancelling is not an option provided.
+function alwaysSucceed(cancelError, cancelerError, cancelerSuccess) {
+  cancelerSuccess();
+};
+
 exports._adminApp= function(databaseName) {
   return function (onError, onSuccess) {
     var app = firebase.initializeAdminApp({ databaseName: databaseName});
     onSuccess(app);
   };
 
-  //canceler always succeeds
-  return function (cancelError, cancelerError, cancelerSuccess) {
-    cancelerSuccess(); // invoke the success callback for the canceler
+  return alwaysSucceed;
+};
+
+exports._initializeTestApp = function(authRecord) {
+  return function (onError, onSuccess) {
+    var app = firebase.initializeTestApp(authRecord);
+    onSuccess(app);
   };
+
+  return alwaysSucceed;
+};
+
+exports._initializeAnonymousTestApp = function(databaseName) {
+  return function (onError, onSuccess) {
+    var authRecord = firebase.initializeTestApp({
+      databaseName: databaseName,
+      auth: { uid: null }
+    });
+
+    var app = firebase.initializeTestApp(authRecord);
+    onSuccess(app);
+  };
+
+  return alwaysSucceed;
 };
--- src/Web/Firebase/Testing.purs
+++ src/Web/Firebase/Testing.purs
@@ -1,18 +1,45 @@
 module Web.Firebase.Testing (
   DatabaseName(..),
+  UserId,
   initializeAdminApp) where
 
 import Effect.Aff (Aff)
 import Effect.Aff.Compat (EffectFnAff, fromEffectFnAff)
-import Prelude ((<<<))
+import Prelude ((<<<), ($))
 import Web.Firebase.Types (FirebaseAppImpl)
 
 newtype DatabaseName = DatabaseName String
+newtype UserId = UserId String  -- | e.g. UserId "alice"
 
 -- | Wrapper for
--- | https://firebase.google.com/docs/database/security/test-rules-emulator
+-- | https://firebase.google.com/docs/database/security/test-rules-emulator .
+-- | Fiunctions have the same name as in @firebase/testing.
+-- | Interfaces are minimal:
+-- | they match the examples, and the purescript types are narrow
+-- | so it is easy to use them safely, without additional conversions
+-- | in test code.
 
 foreign import _adminApp :: DatabaseName -> EffectFnAff FirebaseAppImpl
 
 initializeAdminApp :: DatabaseName -> Aff FirebaseAppImpl
 initializeAdminApp = fromEffectFnAff <<< _adminApp
+
+type AuthRecord = {
+  databaseName:: String,
+  auth:: {uid:: String}
+}
+
+foreign import _initializeTestApp :: AuthRecord -> EffectFnAff FirebaseAppImpl
+
+initializeTestApp :: DatabaseName -> UserId -> Aff FirebaseAppImpl
+initializeTestApp (DatabaseName dbName) (UserId uid) = fromEffectFnAff $
+  _initializeTestApp {
+  databaseName: dbName,
+  auth: {uid}
+}
+
+foreign import _initializeAnonymousTestApp :: DatabaseName -> EffectFnAff FirebaseAppImpl
+-- | Firebase examples pass 'null' for a UID. We don't mimic that,
+-- | in order to keep callers clean.
+initializeAnonymousTestApp :: DatabaseName -> Aff FirebaseAppImpl
+initializeAnonymousTestApp = fromEffectFnAff <<< _initializeAnonymousTestApp
