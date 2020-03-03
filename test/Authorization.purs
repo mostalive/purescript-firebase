61c97548b77db0757ac9251e13e729d90ecf76db
Willem van den Ende <willem@qwan.eu>
2020-03-03 10:41:13 +0000

    Factor out forbidden reference

--- test/Authorization.purs
+++ test/Authorization.purs
@@ -4,44 +4,45 @@ import Data.Either (either)
 import Effect.Aff (Aff, attempt)
 import Effect.Exception (message)
 import Foreign (unsafeToForeign)
-import Prelude (Unit, bind, discard, ($))
-import Test.Spec (Spec, describe, it)
-import Test.Spec.Assertions (expectError, shouldEqual)
+import Prelude (Unit, bind, discard, pure, ($))
+import Test.Spec (Spec, before, describe, it)
+import Test.Spec.Assertions (expectError, shouldEqual, fail)
 import Web.Firebase (EventType(ChildMoved, ChildChanged, ChildRemoved, ChildAdded))
-import Web.Firebase.Aff as FAff
+import Web.Firebase.Aff as DBA
+import Web.Firebase.Types (DatabaseImpl)
 import Web.Firebase.Types as FBT
 
-forbiddenR :: FBT.Firebase -> Aff FBT.Firebase
-forbiddenR = FAff.child "forbidden"
 
 authorizationSpec :: FBT.Firebase -> Spec Unit
-authorizationSpec ref = do
-    describe "Authorization" do
+authorizationSpec root = before (setForbiddenEntry root) do
+    describe "Authorization"    do
       describe "Writing" do
-        it "with Aff push on forbidden location throws an error" do
-          forbiddenRef <- forbiddenR ref
+        it "with Aff push on forbidden location throws an error" $ \forbidden -> do
           let newValue = {success: "push Aff"}
-          expectError $ FAff.push (unsafeToForeign newValue) forbiddenRef
+          expectError $ DBA.push (unsafeToForeign newValue) forbidden
       describe "once() on forbidden location" do
-        it "with Aff throws an error" do
-           forbiddenRef <- forbiddenR ref
-           e <- attempt $ FAff.onceValue forbiddenRef  -- catch error thrown and assert
+        it "with Aff throws an error" $ \forbidden -> do
+           e <- attempt $ DBA.onceValue forbidden  -- catch error thrown and assert
            either (\err -> (message err) `shouldEqual` "permission_denied at /forbidden: Client doesn't have permission to access the desired data.\n | firebase code: | \n PERMISSION_DENIED") (\_ -> "expected an error to be thrown" `shouldEqual` "but was not") e
       describe "on() at forbidden location" do
-        it "ChildAdded with Aff throws an error" do
-          forbiddenRef <- forbiddenR ref
-          expectError $ FAff.on ChildAdded forbiddenRef
-        it "ChildRemoved with Aff throws an error" do
-          forbiddenRef <- forbiddenR ref
-          expectError $ FAff.on ChildRemoved forbiddenRef
-        it "ChildChanged with Aff throws an error" do
-          forbiddenRef <- forbiddenR ref
-          expectError $ FAff.on ChildChanged forbiddenRef
-        it "ChildMoved with Aff throws an error" do
-          forbiddenRef <- forbiddenR ref
-          expectError $ FAff.on ChildMoved forbiddenRef
-      it "set() with Aff at forbidden location throws an error" do
-        forbiddenRef <- forbiddenR ref
+        it "ChildAdded with Aff throws an error" $ \forbidden -> do
+          expectError $ DBA.on ChildAdded forbidden
+        it "ChildRemoved with Aff throws an error" $ \forbidden ->   do
+          expectError $ DBA.on ChildRemoved forbidden
+        it "ChildChanged with Aff throws an error" $ \forbidden -> do
+          expectError $ DBA.on ChildChanged forbidden
+        it "ChildMoved with Aff throws an error" $ \forbidden -> do
+          expectError $ DBA.on ChildMoved forbidden
+      it "set() with Aff at forbidden location throws an error" $ \forbidden ->  do
         let newValue = {success: "set Aff"}
-        e <- attempt $ FAff.set (unsafeToForeign newValue) forbiddenRef
-        either (\err -> (message err) `shouldEqual` "PERMISSION_DENIED: Permission denied\n | firebase code: | \n PERMISSION_DENIED") (\_ -> "expected an error to be thrown" `shouldEqual` "but was not") e
+        e <- attempt $ DBA.set (unsafeToForeign newValue) forbidden
+        either (\err -> (message err) `shouldEqual` "PERMISSION_DENIED: Permission denied\n | firebase code: | \n PERMISSION_DENIED")
+               (\_ -> fail "expected an error to be thrown but was not")
+               e
+
+setForbiddenEntry :: DatabaseImpl -> Aff DatabaseImpl
+setForbiddenEntry root = do
+  forbidden <- DBA.child "forbidden" root
+  pathRoot <- DBA.child "firstchild" forbidden
+  DBA.set (unsafeToForeign "something") pathRoot
+  pure forbidden
